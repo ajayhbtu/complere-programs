8d0f08722b2ac9f4ccae8e4432c02f87c80859a8
ajayhbtu <ajaygupta.hbtu@gmail.com>
2021-12-05 12:32:18 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const logger = require('./lib/logger')();
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Greetings, Server is running!!!');
+});
+
+app.listen(PORT, () => {
+  logger.info(`Server is up on port ${PORT}...`);
+});
